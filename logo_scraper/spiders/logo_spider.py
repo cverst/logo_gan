df4561f1fc18c16a51fb2cf6db12e4d9f5e17b4a
cverst <corstiaen.versteegh@gmail.com>
2021-10-10 21:19:52 +0200

    first data cleaning steps and docstrings

--- logo_scraper/spiders/logo_spider.py
+++ logo_scraper/spiders/logo_spider.py
@@ -1,10 +1,18 @@
 import scrapy
 import csv
 import urllib
-import os
 
 
-def get_start_urls():
+def get_start_urls() -> list:
+    """Return list of start_urls for LogoSpider
+
+    Returns:
+        list: ["url_1",
+               "url_2",
+               ...,
+               "url_n]
+    """
+
     # read company list from .csv
     companies = []
     with open("Forbes-2021.csv", "r", encoding="ISO-8859-1") as csvfile:
@@ -12,20 +20,23 @@ def get_start_urls():
         next(csvreader)  # skip header
         for row in csvreader:
             companies.append(row[1])
-    
+
     # create Wikipedia urls, some will not work, fix this later
-    url_start = 'https://en.wikipedia.org/wiki/'
-    start_urls = [urllib.parse.urljoin(url_start, comp.replace(' ', '_')) for comp in  companies]
+    url_start = "https://en.wikipedia.org/wiki/"
+    start_urls = [
+        urllib.parse.urljoin(url_start, comp.replace(" ", "_")) for comp in companies
+    ]
 
     return start_urls
 
 
-def image_full_filename(image_filename):
-        full_filename = os.path.join(os.getcwd(), 'logo_images', image_filename)
-        return full_filename
-
-
 class LogoItem(scrapy.Item):
+    """LogoSpider output class
+
+    Args:
+        scrapy.Item: inherits from scrapy.Item class
+    """
+
     company_name = scrapy.Field()
     wiki_name = scrapy.Field()
     wiki_url = scrapy.Field()
@@ -34,23 +45,40 @@ class LogoItem(scrapy.Item):
 
 
 class LogoSpider(scrapy.Spider):
-    name = 'logo'
+    """The spider retrieves logos of Wikipedia entries of companies in the Forbes
+    2000 (2021) list. The Wikipedia URLs constructed from the Forbes 2000
+    company names are not always leading to the corporation, and not all
+    Wikipedia entries follow the scraping scheme used here. About 700 out of
+    2000 companies therefore have no logo retrieved. Catching and correcting
+    the errors is tedious so we accept the smaller, decently sized dataset.
+    The list of images is manually checked for and pruned of erroneous images
+    (e.g., buildings, all black images).
+
+    Args:
+        scrapy.Spider: inherits from scrapy.Spider class
+
+    Yields:
+        LogoItem class instances stored as logo_links.json and images stored
+        in ./full. The LogoItem class instances contain references to the
+        image locations.
+    """
+
+    name = "logo"
 
     def start_requests(self):
         return [scrapy.http.Request(url=start_url) for start_url in get_start_urls()]
 
     def parse(self, response):
-        
+
         logo = LogoItem()
 
-        logo['wiki_url'] = response._get_url()
-        logo['company_name'] = logo['wiki_url'].split('/')[-1].replace('_', ' ')
-        
-        info_items = response.css('table.infobox.vcard')
-        logo['wiki_name'] = info_items.css('caption.infobox-title.fn.org::text').get()
+        logo["wiki_url"] = response._get_url()
+        logo["company_name"] = logo["wiki_url"].split("/")[-1].replace("_", " ")
 
-        url_part = response.css('td.infobox-image.logo').css('img').attrib['src']
-        logo['image_urls'] = [urllib.parse.urljoin('https:', url_part)]
+        info_items = response.css("table.infobox.vcard")
+        logo["wiki_name"] = info_items.css("caption.infobox-title.fn.org::text").get()
+
+        url_part = response.css("td.infobox-image.logo").css("img").attrib["src"]
+        logo["image_urls"] = [urllib.parse.urljoin("https:", url_part)]
 
         yield logo
-
